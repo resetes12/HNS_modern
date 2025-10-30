bafcb972d785e6598041615c16114dda24a5172c
dylanfalzone1 <dylanfalzone31@gmail.com>
2025-10-30 18:43:51 -0400

    Updated lightsprite 4 to appear behind player

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -2752,7 +2752,8 @@ static void SpawnLightSprite(s16 x, s16 y, s16 camX, s16 camY, u32 lightType) {
     case 4: // Lighthouse and Glow Lichen
         sprite->centerToCornerVecX = -(32 >> 1);
         sprite->centerToCornerVecY = -(32 >> 1);
-        sprite->oam.priority = 1;
+        sprite->oam.priority = 2;
+        sprite->subpriority = 0xFF;
         sprite->oam.objMode = 1; // BLEND
         sprite->oam.affineMode = ST_OAM_AFFINE_NORMAL;
         sprite->x += 8;
