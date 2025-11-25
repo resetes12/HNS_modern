2d32fea515d889fa03d194b3ed585c91351a6024
resetes12 <diploporigon@gmail.com>
2025-11-25 16:19:28 +0100

    Fixed partner battle names (Includes Lance partner battle)

--- src/battle_message.c
+++ src/battle_message.c
@@ -2864,20 +2864,7 @@ u32 BattleStringExpandPlaceholders(const u8 *src, u8 *dst)
                 break;
             case B_TXT_PARTNER_NAME:
                 GetFrontierTrainerName(text, gPartnerTrainerId);
-                #ifdef BATTLE_ENGINE
-                    if (gPartnerSpriteId == TRAINER_BACK_PIC_BRENDAN
-                      || gPartnerSpriteId == TRAINER_BACK_PIC_MAY)
-                    {
-                        toCpy = gSaveBlock2Ptr->rivalName;
-                    }
-                    else
-                    {
-                        GetFrontierTrainerName(text, gPartnerTrainerId);
-                        toCpy = text;
-                    }
-                #else
-                    toCpy = gTrainers[TRAINER_MAY_ROUTE_103_MUDKIP].trainerName;
-                #endif
+                toCpy = text;
                 break;
             case B_TXT_RIVAL_NAME:
                 toCpy = gSaveBlock2Ptr->rivalName;
