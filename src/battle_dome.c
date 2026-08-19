06465ec5f923ced8a4d2fc4fcecf3e2553ef2a69
resetes12 <diploporigon@gmail.com>
2026-08-19 15:50:36 +0200

    Made Gorebyss WTR/PSY and Huntail WTR/DRK.

--- src/battle_dome.c
+++ src/battle_dome.c
@@ -2450,9 +2450,10 @@ static void InitDomeTrainers(void)
                 || species[j] == SPECIES_MASQUERAIN
                 || species[j] == SPECIES_DELCATTY
                 || species[j] == SPECIES_GULPIN
-                || species[j] == SPECIES_SWALOT
-                || species[j] == SPECIES_LUVDISC
-                || species[j] == SPECIES_ELECTIVIRE))
+                || species[j] == SPECIES_ELECTIVIRE
+                || species[j] == SPECIES_YANMEGA
+                || species[j] == SPECIES_HUNTAIL
+                || species[j] == SPECIES_GOREBYSS))
                 {
                     monTypesBits |= gBitTable[gSpeciesInfo[gFacilityTrainerMons[DOME_MONS[i][j]].species].types_old[0]];
                     monTypesBits |= gBitTable[gSpeciesInfo[gFacilityTrainerMons[DOME_MONS[i][j]].species].types_old[1]];
