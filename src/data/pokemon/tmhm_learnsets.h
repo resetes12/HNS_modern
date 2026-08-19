5080f497ca766785e7d1151d4f6a8ef5f0e1cb68
resetes12 <diploporigon@gmail.com>
2026-08-19 15:48:34 +0200

    Added Brick Break to Delibird

--- src/data/pokemon/tmhm_learnsets.h
+++ src/data/pokemon/tmhm_learnsets.h
@@ -5878,6 +5878,7 @@ const union {
         .RAIN_DANCE = TRUE,
         .FRUSTRATION = TRUE,
         .RETURN = TRUE,
+        .BRICK_BREAK = TRUE,
         .DOUBLE_TEAM = TRUE,
         .AERIAL_ACE = TRUE,
         .FACADE = TRUE,
