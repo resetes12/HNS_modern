437cd4384f546e52efc5b3d177f4226f700ee03b
resetes12 <diploporigon@gmail.com>
2026-08-19 15:56:43 +0200

    Pkm cry plays when coming back to the INFO screen in the Pokedex

--- src/pokedex_plus_hgss.c
+++ src/pokedex_plus_hgss.c
@@ -4262,6 +4262,7 @@ void OpenPokedexInfoScreen(u16 species, void (*returnCallback)(void))
 //*        Area screen               *
 //*                                  *
 //************************************
+#define tSkipCry         data[3]
 static void Task_LoadAreaScreen(u8 taskId)
 {
     switch (gMain.state)
@@ -4308,6 +4309,7 @@ static void Task_SwitchScreensFromAreaScreen(u8 taskId)
         {
         case 1:
         default:
+            gTasks[taskId].tSkipCry = FALSE;
             gTasks[taskId].func = Task_LoadInfoScreen;
             break;
         case 2:
@@ -4347,6 +4349,7 @@ static void Task_ExitAreaScreenToExternal(u8 taskId)
     }
 }
 
+#undef tSkipCry
 
 
 //************************************
