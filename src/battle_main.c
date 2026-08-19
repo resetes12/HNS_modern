be5de3c625372e1cea24b6af754fad25ab2512ac
resetes12 <diploporigon@gmail.com>
2026-08-19 15:36:09 +0200

    Fix 2x battle speed breaking palette fades (e.g. Hyper Beam, by @aloven)

--- src/battle_main.c
+++ src/battle_main.c
@@ -2264,7 +2264,8 @@ void BattleMainCB2(void)
 
     if (gSaveBlock2Ptr->optionsBattleSpeed
         && !(gBattleTypeFlags & BATTLE_TYPE_LINK)
-        && !gBattleCaptureSuccessActive)
+        && !gBattleCaptureSuccessActive
+        && !gPaletteFade.active)
     {
         bool8 ballActive = FALSE;
         u8 i;
