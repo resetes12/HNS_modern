166bfea560f190335c8d11c4161c999502dfacca
resetes12 <diploporigon@gmail.com>
2026-09-02 15:57:19 +0200

    Improved Oak Intro

--- include/strings.h
+++ include/strings.h
@@ -375,6 +375,7 @@ extern const u8 gText_Birch_SoItsPlayer[];
 extern const u8 gText_Birch_YourePlayer[];
 extern const u8 gText_Birch_ChallengesSoundRight[];
 extern const u8 gText_Birch_AreYouReady[];
+extern const u8 gText_Birch_ChallengeSelected[];
 extern const u8 gText_ContinueMenuPlayer[];
 extern const u8 gText_ContinueMenuTime[];
 extern const u8 gText_ContinueMenuPokedex[];
